e1055c5c2ebf2c2dfa18e41ecc2921339c0c29a5
AtulyaJaiswal <jaiswalatulya.aj@gmail.com>
2023-03-19 22:57:35 +0530

    small update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -56,46 +56,46 @@ exports.logout = catchAsyncErrors(async (req,res,next) =>{
     });
 });
 
-//FORGOT PASSWORD
-exports.forgotPassword = catchAsyncErrors(async (req,res,next) => {
-    const user = await User.findOne({email: req.body.email});
+// //FORGOT PASSWORD
+// exports.forgotPassword = catchAsyncErrors(async (req,res,next) => {
+//     const user = await User.findOne({email: req.body.email});
 
-    if(!user){
-        return next(new ErrorHandler("User not found", 404));
-    }
+//     if(!user){
+//         return next(new ErrorHandler("User not found", 404));
+//     }
 
-    //GET ResetPasswordToken
-    const resetToken = user.getResetPasswordToken();
+//     //GET ResetPasswordToken
+//     const resetToken = user.getResetPasswordToken();
 
-    await user.save({validateBeforeSave: false});
+//     await user.save({validateBeforeSave: false});
 
-    const resetPasswordUrl = `${req.protocol}://${req.get("host")}/password/reset/${resetToken}`;    
+//     const resetPasswordUrl = `${req.protocol}://${req.get("host")}/password/reset/${resetToken}`;    
     
-   //MESSAGE TO BE SENT ON EMAIL
-   const message = `Your Password reset token is :- \n
-   \n ${resetPasswordUrl} \n
-   \n If you have not requested this email, please ignore it`;
+//    //MESSAGE TO BE SENT ON EMAIL
+//    const message = `Your Password reset token is :- \n
+//    \n ${resetPasswordUrl} \n
+//    \n If you have not requested this email, please ignore it`;
    
-   try {
+//    try {
 
-        await sendEmail({
-            email: user.email,
-            subject: `Password recovery`,
-            message,
-        });
-        res.status(200).json({
-            success:true,
-            message: `Email sent to ${user.email} successfully`
-        });
+//         await sendEmail({
+//             email: user.email,
+//             subject: `Password recovery`,
+//             message,
+//         });
+//         res.status(200).json({
+//             success:true,
+//             message: `Email sent to ${user.email} successfully`
+//         });
     
-   } catch (error) {
-        user.resetPasswordToken=d=undefined;
-        user.resetPasswordExpire=undefined;
-        await user.save({validateBeforeSave: false});
+//    } catch (error) {
+//         user.resetPasswordToken=d=undefined;
+//         user.resetPasswordExpire=undefined;
+//         await user.save({validateBeforeSave: false});
 
-        return next(new ErrorHandler(error.message, 500));
-   }
-});
+//         return next(new ErrorHandler(error.message, 500));
+//    }
+// });
 
 
 //RESET PASSWORD
