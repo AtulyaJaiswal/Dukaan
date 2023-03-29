beeb2d299052eb94f31d812ec8f666f08ba852eb
AtulyaJaiswal <jaiswalatulya.aj@gmail.com>
2023-03-29 16:03:23 +0530

    Checking Refresh2

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ app.use("/api/v1", payment);
 
 // USE ON DEPLOYMENT
 app.use(express.static(path.join(__dirname, "../frontend/build/index.html")));
-app.get("*", (req,res) => {
+app.get("/*", (req,res) => {
     res.sendFile(path.resolve(__dirname, "../frontend/build"))
 });
 
